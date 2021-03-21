87cbcb966504c64a3de36dd9dd57961e5bded9b0
Nelson Tran <ntran@cadmakers.com>
2021-03-21 14:29:12 -0700

    add typescript config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Nelson Tran`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `Nelson Tran`,
+    email: `nelstran@gmail.com`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
---
+++ src/components/Layout.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+interface Props {}
+
+export const Layout: React.FC = () => {
+  return <div></div>
+}
