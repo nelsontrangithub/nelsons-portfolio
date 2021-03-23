90da9a40c606c6f853d87325e26261e0484325f9
Nelson Tran <ntran@cadmakers.com>
2021-03-23 00:24:55 -0700

    test

---
+++ .cache/sync-requires.js
@@ -0,0 +1,9 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---src-pages-index-tsx": preferDefault(require("C:\\Users\\Nelson Tran\\Desktop\\portfolio\\portfolio-dev\\src\\pages\\index.tsx"))
+}
+
