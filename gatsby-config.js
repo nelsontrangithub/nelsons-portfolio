808491359869a560aa2818eeb72bbf45375b4db9
Nelson Tran <nelsontran@icloud.com>
2025-12-16 22:53:52 -0800

    Upgrade to Gatsby 5 and React 18

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-sass`,
-    `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
