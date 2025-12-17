98754c489e32afa8b36d9c72cb38b6175c3730aa
Nelson Tran <nelsontran@icloud.com>
2025-12-16 23:09:39 -0800

    Switch to Inter font

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,7 @@
  * See: https://www.gatsbyjs.com/docs/browser-apis/
  */
 
-// You can delete this file if you're not using it
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
