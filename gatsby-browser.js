28e80194c0a1d34aff84c4de2bd9873c04edf894
Nelson Tran <nelsontran@icloud.com>
2025-12-18 23:22:18 -0800

    Add Vercel Analytics

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,7 +4,17 @@
  * See: https://www.gatsbyjs.com/docs/browser-apis/
  */
 
+import React from "react";
+import { Analytics } from "@vercel/analytics/react";
+
 import "@fontsource/inter/400.css";
 import "@fontsource/inter/500.css";
 import "@fontsource/inter/600.css";
 import "@fontsource/inter/700.css";
+
+export const wrapRootElement = ({ element }) => (
+  <>
+    {element}
+    <Analytics />
+  </>
+);
